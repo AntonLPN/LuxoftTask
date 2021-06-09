801513f3c630a703e6ea759f36640700b900d661
AntonLPN <68461689+AntonLPN@users.noreply.github.com>
2021-06-10 00:10:24 +0300

    Добавьте файлы проекта.

---
+++ LuxoftTask/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Technical question:
+//Write a method that returns the top 'x' elements from a list.
+//Example: Call of YourClass.LargestX(3, new List<int> { 12, 9, 5, 4, 3, 2, 0 })  => should return new List<int> { 5, 9, 12 }
+namespace LuxoftTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Task\nWrite a method that returns the top 'x' elements from a list.\nExample: Call of \nYourClass.LargestX(3, new List<int> { 12, 9, 5, 4, 3, 2, 0 })  => should return new List<int> { 5, 9, 12 }");
+            Console.WriteLine();
+            myClass listTest = new myClass();
+            List<int> list =new List<int> { 12, 9, 5, 4, 3, 2, 0 };
+            Console.WriteLine("Displaying a preliminary list");
+            Console.WriteLine("-----------------------------------------------------");
+            foreach (var item in list)
+            {
+                Console.Write($"{item},");
+            }
+           
+            try
+            {
+                int count = 3;
+              
+
+                //main method***************************
+                list = listTest.LargestX(count, list);
+                //*************************************
+
+
+                Console.WriteLine("\n-----------------------------------------------------");
+                Console.WriteLine("Output of the method in the specified order and quantity from the task");
+                foreach (var item in list)
+                {
+                    Console.Write($"{item},");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("\n\nError\n" +ex.Message);
+            }
+
+            Console.WriteLine();
+            Console.Read();
+           
+          
+        }
+    }
+}
---
+++ LuxoftTask/myClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxoftTask
+{
+    public class myClass
+    {
+
+       public myClass()
+        {
+
+        }
+
+        /// <summary>
+        /// The method sorts from smallest
+        /// to largest number of the list.
+        /// Outputs a user-defined number of items
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public List<int> LargestX(int count, List<int> list)
+        {
+            List<int> result = new List<int>();
+
+            list.Sort();
+            list.Reverse();
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(list[i]);
+            }
+            result.Reverse();
+
+            return result ;
+
+        }
+
+
+
+
+    }
+}
